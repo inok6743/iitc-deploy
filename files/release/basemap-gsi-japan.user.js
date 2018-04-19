24902664854a0dff209b1f30adfe077c886717a6
Travis CI (Automatic Commit) <inok6743@gmail.com>
2018-04-19 12:32:25 +0000

    IITC: Automatic deploy from Travis-CI (#44)

---
+++ files/release/basemap-gsi-japan.user.js
@@ -0,0 +1,103 @@
+// ==UserScript==
+// @id             iitc-plugin-basemap-gsi-japan
+// @name           IITC plugin: GSI map tiles (Japan Only)
+// @category       Map Tiles
+// @version        0.2.5.20180419.123209
+// @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
+// @updateURL      none
+// @downloadURL    none
+// @description    [local-2018-04-19-123209] Add the map tiles provided by Geospatial Information Authority of Japan as optional layers. Available only in Japan.
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @include        https://*.ingress.com/mission/*
+// @include        http://*.ingress.com/mission/*
+// @match          https://*.ingress.com/mission/*
+// @match          http://*.ingress.com/mission/*
+// @grant          none
+// ==/UserScript==
+
+
+function wrapper(plugin_info) {
+// ensure plugin framework is there, even if iitc is not yet loaded
+if(typeof window.plugin !== 'function') window.plugin = function() {};
+
+//PLUGIN AUTHORS: writing a plugin outside of the IITC build environment? if so, delete these lines!!
+//(leaving them in place might break the 'About IITC' page or break update checks)
+plugin_info.buildName = 'local';
+plugin_info.dateTimeVersion = '20180419.123209';
+plugin_info.pluginId = 'basemap-gsi-japan';
+//END PLUGIN AUTHORS NOTE
+
+
+
+// PLUGIN START ////////////////////////////////////////////////////////
+
+// Map data © 国土地理院 (The Geospatial Information Authority of Japan)
+//
+// The bathymetric contours are derived from those contained within the GEBCO
+// Digital Atlas, published by the BODC on behalf of IOC and IHO (2003)
+// (http://www.gebco.net)
+//
+// 海上保安庁許可第２２２５１０号（水路業務法第２５条に基づく類似刊行物）
+//
+// GSI's terms of use: http://www.gsi.go.jp/ENGLISH/page_e30286.html
+//
+// > The Terms of Use are compatible with the Creative Commons Attribution
+// > License 4.0 (hereinafter referred to as the CC License). This means that
+// > Content based on the Terms of Use may be used under the CC License in
+// > lieu of the Terms of Use.
+
+// use own namespace for plugin
+window.plugin.mapTileGsiJapan = {
+  addLayers: function() {
+
+    // Register the GSI map tiles as base layers.
+
+    var basicOptions = {
+      minZoom:       5,
+      maxZoom:       21,
+      maxNativeZoom: 18,
+      detectRetina:  true
+    };
+    var layerAttributes = [
+      { layerName: 'GSI of Japan (Standard)', tileName: '標準地図', directory: 'std'  },
+      { layerName: 'GSI of Japan (Pale)',     tileName: '淡色地図', directory: 'pale' },
+    ];
+
+    layerAttributes.forEach(function (attr) {
+      layerChooser.addBaseLayer(
+        new L.TileLayer(
+          'https://cyberjapandata.gsi.go.jp/xyz/' + attr.directory + '/{z}/{x}/{y}.png',
+          $.extend(basicOptions, {
+            attribution: '<a href="https://maps.gsi.go.jp/development/ichiran.html">'
+                         + '地理院タイル (' + attr.tileName + ')</a>',
+          })
+        ),
+        attr.layerName
+      );
+    });
+  },
+};
+
+var setup = window.plugin.mapTileGsiJapan.addLayers;
+
+// PLUGIN END //////////////////////////////////////////////////////////
+
+
+
+setup.info = plugin_info; //add the script info data to the function as a property
+if(!window.bootPlugins) window.bootPlugins = [];
+window.bootPlugins.push(setup);
+// if IITC has already booted, immediately run the 'setup' function
+if(window.iitcLoaded && typeof setup === 'function') setup();
+} // wrapper end
+// inject code into site context
+var script = document.createElement('script');
+var info = {};
+if (typeof GM_info !== 'undefined' && GM_info && GM_info.script) info.script = { version: GM_info.script.version, name: GM_info.script.name, description: GM_info.script.description };
+script.appendChild(document.createTextNode('('+ wrapper +')('+JSON.stringify(info)+');'));
+(document.body || document.head || document.documentElement).appendChild(script);
+
+
---
+++ files/release/force-https.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @id             force-https@jonatkins
+// @name           IITC plugin: force https access for ingress.com/intel
+// @category       Tweaks
+// @version        0.1.0.20180419.123209
+// @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
+// @updateURL      none
+// @downloadURL    none
+// @description    [local-2018-04-19-123209] Force https access for the intel map. If the intel map is accessed via http, it redirects to the https version.
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @include        https://*.ingress.com/mission/*
+// @include        http://*.ingress.com/mission/*
+// @match          https://*.ingress.com/mission/*
+// @match          http://*.ingress.com/mission/*
+// @grant          none
+// ==/UserScript==
+
+
+
+//NOTE: plugin authors - due to the unique requirements of this plugin, it doesn't use the standard IITC
+//plugin architecture. do NOT use it as a template for other plugins
+
+
+if(window.location.protocol !== 'https:') {
+  var redir = window.location.href.replace(/^http:/, 'https:');
+  window.location = redir;
+  throw('Need to load HTTPS version.');
+}
